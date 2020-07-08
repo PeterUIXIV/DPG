e68f77eaa13fa6891e7bc36860ebb7fa9c394879
PeterMaier <peter.maier@maierfun.de>
2020-07-08 12:00:51 +0200

    normalized rewards

--- zooming.py
+++ zooming.py
@@ -7,22 +7,20 @@ import math
 import itertools
 import pandas as pd
 import portion as P
+import statistics
 
 style.use("ggplot")
 
-SHOW_EVERY = 500  # how often to play through env visually.
-show = False
-test_pricing = False
+# SHOW_EVERY = 1000  # how often to play through env visually.
 
+lower = 0.01
 
-action_space = ['0', '0.025', '0.05', '0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.8', '1']
+# action_space = ['0', '0.025', '0.05', '0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.8', '1']
 
 
 class Agent:
-    def __init__(self, beta, sigma_square, learning_rate=0.1, reward_decay=0.95, eps_greedy=0.95, eps_decay=0.9995,
-                 mu=0, value_function='action_value_method'):
+    def __init__(self, beta, sigma_square, mu=0):
         # value functions: action_value_method, q-learning, UCB
-        self.value_function = value_function
 
         self.mu = mu
         self.beta = beta
@@ -31,13 +29,21 @@ class Agent:
         self.active_arms = []
         self.pulled_arms = []
         self.avg_rewards = []
+        self.max_profit = 0
+        self.min_profit = 0
+        self.high_count = 0
+        self.low_count = 0
+
+        self.current_action = 0
 
     def choose_action(self, i_ph):
         # Activation rule
-        not_covered = P.closed(lower=0, upper=1)
+        # not_covered = P.closed(lower=0, upper=1)
+        not_covered = P.closed(lower=0.01, upper=self.sigma_square)
+        # scale = not_covered.upper - not_covered.lower
         for i in range(len(self.active_arms)):
+            # confidence_radius = scale * self.confidence_radius(i_ph, i)
             confidence_radius = self.confidence_radius(i_ph, i)
-            # confidence_radius = math.sqrt((8 * i_ph)/1 + self.pulled_arms[i])
             confidence_interval = P.closed(self.active_arms[i] - confidence_radius, self.active_arms[i] + confidence_radius)
             not_covered = not_covered - confidence_interval
             if show:
@@ -54,6 +60,7 @@ class Agent:
                 heights.append(i.upper - i.lower)
             ran_n = np.random.uniform(0, height)
             j = 0
+            ran = 0
             for i in range(len(heights)):
                 if j < ran_n < j + heights[i]:
                     ran = rans[i]
@@ -74,13 +81,35 @@ class Agent:
                 max_index = index
                 max_index_arm = i
         action = self.active_arms[max_index_arm]
+        # self.current_action = action
+        # action = action * (self.sigma_square - lower) + lower
         return action
 
     def learn(self, action, reward):
-        # TODO: testing
-        # reward = (reward + 200)/(270 + 200)
+        # action = (action - lower)/(self.sigma_square - lower)
+        # action = self.current_action
+        if reward > self.max_profit:
+            self.max_profit = reward
+        elif reward < self.min_profit:
+            self.min_profit = reward
         if show:
-            print(f"reward: {reward}")
+            print(f"action: {action}, reward: {reward}")
+        # TODO: testing
+
+        high = 100
+        low = -75
+        if reward >= high:
+            reward = 1
+            self.high_count += 1
+        elif reward <= low:
+            reward = 0
+            self.low_count += 1
+        else:
+            reward = (reward - low)/(high - low)
+
+        # reward = reward / 20
+        if show:
+            print(f"high_count: {self.high_count}, low_count: {self.low_count}")
         arm = self.active_arms.index(action)
         self.avg_rewards[arm] = (self.pulled_arms[arm] * self.avg_rewards[arm] + reward) / (self.pulled_arms[arm] + 1)
         self.pulled_arms[arm] += 1
@@ -90,6 +119,7 @@ class Agent:
 
 
 class DPG:
+
     def __init__(self, agents, sigma_square=1, r_max=15, pricing_mechanism='LOO'):
 
         # self.beta = np.array([5, -3])
@@ -102,6 +132,10 @@ class DPG:
         self.episode_rewards = []
         self.episode_rewards_sum = []
 
+    '''
+    def max_radius(self, z=4.891638, agents):
+        x_s = np.array(z*agents.beta)
+    '''
     def v(self, indices, y_tilde_s, y):
         sum_y_tilde_s = 0
         for i in indices:
@@ -136,8 +170,8 @@ class DPG:
         for i in range(n):
             x_s[i] = np.random.normal(self.agents[i].mu, self.agents[i].sigma_square ** .5)
             if test_pricing:
-                x_s[0] = 0.055664941
-                x_s[1] = 0.435856935
+                x_s[0] = -0.473797659
+                x_s[1] = 0.121024593
 
             if float(actions[i]) == 0:
                 s_square_s[i] = self.agents[i].sigma_square
@@ -147,14 +181,14 @@ class DPG:
                 x_tilde_s[i] = x_s[i] + np.random.normal(0, s_square_s[i] ** .5)
 
             if test_pricing:
-                x_tilde_s[0] = 0.463488408
-                x_tilde_s[1] = 0.282960863
+                x_tilde_s[0] = -0.426597148
+                x_tilde_s[1] = 0.650116791
             y_tilde_s[i] = x_tilde_s[i] * self.agents[i].beta
             # y_tilde = sum(x_tilde_s * self.beta)
 
         epsilon = np.random.normal(0, self.sigma_square ** .5)
         if test_pricing:
-            epsilon = 0.047803865
+            epsilon = 0.10286918
 
         y = sum(x_s[i] * agents[i].beta for i in range(len(self.agents))) + epsilon
         # y = sum(x_s * self.beta) + epsilon
@@ -199,6 +233,8 @@ class DPG:
 
                 for j in range(len(agents)):
                     # print(f"player {j} action: {actions[j]} profit: {rewards[j]}")
+                    if show:
+                        print(f"agent: {j}")
                     self.agents[j].learn(actions[j], rewards[j])
                     # print(f"{type(action)}, {action}")
                     # print(action)
@@ -206,19 +242,23 @@ class DPG:
                 episode_reward = sum(rewards)
                 self.episode_rewards_sum.append(episode_reward)
 
-                if t % SHOW_EVERY == 0:
-                    global show
-                    show = True
-                    # print(f"Episode: #{episode}")
+                if (t * i_ph) % SHOW_EVERY == 0:
+                    globals()['show'] = True
+                    rewards_per_agent = list(zip(*self.episode_rewards))
+                    # avg_rewards_per_agent = []
+                    # for list_of_rewards in rewards_per_agent:
+                    #     avg_rewards_per_agent.append(statistics.mean(list_of_rewards))
                     print()
-                    print(f"Episode {t}, {SHOW_EVERY} episode mean: {np.mean(self.episode_rewards_sum[-SHOW_EVERY:])}")
+                    print(f"Episode {t} of {2 ** rounds + 1}, {SHOW_EVERY} episode mean: {np.mean(self.episode_rewards_sum[-SHOW_EVERY:])}")
                     for j in range(len(agents)):
                         print(f"player: {j} action: {actions[j]} profit: {rewards[j]}")
+                        print(f"max_profit: {agents[j].max_profit}, min_profit: {agents[j].min_profit}")
                         print(f"active_arms: {agents[j].active_arms}")
                         print(f"pulled_arms: {agents[j].pulled_arms}")
-                        print(f"avg_rewards: {agents[j].avg_rewards}")
+                        print(f"avg_learning_rewards: {agents[j].avg_rewards}")
+                        # print(f"avg_real_rewards: {avg_rewards_per_agent[j]}")
                 else:
-                    show = False
+                    globals()['show'] = False
         moving_avg_sum = np.convolve(self.episode_rewards_sum, np.ones((SHOW_EVERY,)) / SHOW_EVERY, mode='valid')
 
         # moving_avg = []
@@ -241,6 +281,11 @@ class Arm:
 
 
 if __name__ == "__main__":
+    rounds = 16
+    SHOW_EVERY = int((2 ** rounds + 1)/10)
+
+    show = False
+    test_pricing = False
     # training
     p0 = Agent(beta=5, sigma_square=0.5, mu=0)
     p1 = Agent(beta=-3, sigma_square=0.3, mu=0)
@@ -248,6 +293,6 @@ if __name__ == "__main__":
     p3 = Agent(beta=2, sigma_square=0.1, mu=0)
     agents = [p0, p1]
 
-    dpg = DPG(agents, pricing_mechanism='shapley')
+    dpg = DPG(agents, pricing_mechanism='LOO')
     print("training ...")
-    dpg.zoom(12)
+    dpg.zoom(rounds)
